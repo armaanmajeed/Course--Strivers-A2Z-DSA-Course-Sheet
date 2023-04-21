df64d4f30bab978692e83cd26195949e6d855349
Armaan <armaanmajeed.khan@gmail.com>
2023-04-21 16:43:38 +0530

    Patterns

---
+++ Patterns/pattern13.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+class Solution {
+
+    void printTriangle(int n) {
+        int k = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(k + " ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
